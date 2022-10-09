0a44efec8032f241b9f5df5860ccc6b8211bb5ea
Farouk HADJ NACER <73796490+Farouk96@users.noreply.github.com>
2022-10-09 14:25:59 +0100

    Update densenet.py

--- densenet.py
+++ densenet.py
@@ -5,7 +5,7 @@ import torchvision.models as models
 class DenseNet(nn.Module):
     def __init__(self, pretrained=True):
         super(DenseNet, self).__init__()
-        num_classes = 13
+        num_classes = 107
         self.model = models.densenet201(pretrained=pretrained)
         self.model.classifier = nn.Linear(1920, num_classes)
         
